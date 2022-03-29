705e0c6f63f238ba52f495dca1759fbfaba8fd3c
Katherine Owens <22736388+lacyk3@users.noreply.github.com>
2022-03-28 19:29:41 -0700

    Update BarcodeMethods.py
    Generalize Rename() function to use any column titles in new name and allow custom delimiters

--- src/PyPl8/Barcodes/BarcodeMethods.py
+++ src/PyPl8/Barcodes/BarcodeMethods.py
@@ -16,45 +16,59 @@ def get_field (exif,field):
         if TAGS.get(k) == field:
             return v
 
-def rename_images(image_folder_path, mastersheet, code=''):
+def Rename(image_folder_path, mastersheet, columns, code = '', delimiter = '_', datetime_delimiter = '-', datetime = True):
     '''
-    Input: -file path to folder containing images to be renamed. eg. r'C:User\\Documents\\Image_Folder'
-           -file name of .csv containing the information to be used in the new file name. 
-           Required column titles are 'Barcode', 'Plate', and 'Condition'
-           -string that is to be included in new image name as short hand for the folder. Default is blank.
+    Input: -image_folder_path: file path to folder containing images to be renamed inplace. eg. r'C:User\\Documents\\Image_Folder'
+           -mastersheet: file name of .xlsx or .csv containing the information to be used in the new file name. The barcode column must come             first and it must have the title 'Barcode'
+           -columns: list strings indicating the column titles of which columns contain information to be included in the name, eg.
+           ['Plate', 'Condition', 'Replicate']
+           -code: Additional placeholder for other info to be included in the name. Could be short hand for the folder of images eg '24h'. 
+           Default is blank.
     Output: None
+            -datetime indicates whether the timestamp should be included in the image name. The default is True, but it can be switched to
+            False
     
     Running this function with the master sheet in the current working directory will result in renaming the 
-    .jpg files within the input folder as 'Plate_Condition_code_hr-min-sec.jpg' where the appropriate plate 
-    and condition info are determined by decoding the barcode within the image and matching the result with 
-    the appropriate row in mastersheet.
+    .jpg files within the image_folder_path as 'col1_col2_...colN_code_hr-min-sec.jpg'. The appropriate strings from each column are
+    determined by decoding the barcode within the image and matching the result with the appropriate row in mastersheet. Instead of underscores, a different delimiter can be set with the kwarg delimiter, and instead of hyphens a different delimiter for the date-time info can be set with kwarg datetime_delimiter
     '''
-    df = pd.read_excel (mastersheet)
+    if 'csv' in mastersheet:
+        df = pd.read_csv(mastersheet)
+    else:
+        df = pd.read_excel (mastersheet)
     os.chdir(image_folder_path)
     s = df['Barcode']
     image_filenames = glob.glob('*.jpg')
     
     for filename in image_filenames:
-        image = Image.open(filename)
-        im = np.array(image)
-        all_the_info = decode(im)
-        if not all_the_info:
+        try:
+            image = Image.open(filename)
+            im = np.array(image)
+            all_the_info = decode(im)
+            if not all_the_info:
+                print('Barcode could not be detected and/or decoded for image ' + filename)
+                continue
+            barcode = all_the_info[0].data
+            d = barcode.decode('ASCII')
+            image_row = df.loc[s==int(d)]
+            info = image_row.iloc[0]
+            temp = []
+            for c in columns:
+                temp.append(info[c])
+            if len(code)>0:
+                temp.append(code)
+            if datetime:
+                exif_data = image._getexif()
+                DT = get_field(exif_data,'DateTime')
+                Time = DT.split()[1]
+                Time = Time.replace(':',datetime_delimiter)
+                temp.append(Time)
+
+            new_name = delimiter.join(temp) + '.jpg'
+            print(new_name)
+            dst = new_name
+            src = filename
+            os.rename(src,dst)
+        except:
+            print('Error processing image ' + filename + '. Possibly barcode ' + d+ 'not found in mastersheet.')
             continue
-        barcode = all_the_info[0].data
-        d = barcode.decode('ASCII')
-        image_row = df.loc[s==int(d)]
-        print(image_row)
-        info = image_row.iloc[0]
-        exif_data = image._getexif()
-        DT = get_field(exif_data,'DateTime')
-        Time = DT.split()[1]
-        Time = Time.replace(':','-')
-        new_name = info.Plate+'_'+info.Condition+'_'+str(info.Replicate)+'_'+code+'_'+Time+'.jpg'
-        dst = new_name
-        src = filename
-        os.rename(src,dst)
-        
-def identityfxn(a):
-    plt.plot(a,a)
-    plt.show()
-    return a
